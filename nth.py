c35d795e0645b6da838e501d5316c48548af3496
Ioulianos Kakoulidis <ioulianos.kakoulidis@hotmail.com>
2018-03-11 15:59:17 +0200

    phi

--- nth.py
+++ nth.py
@@ -23,6 +23,20 @@ def sieve_sundaram(n):
             sieve[j] = 1
     return [2] + [2*i+1 for i in xrange(1, ub) if sieve[i] == 0]
 
+def totient(n):
+    size = n + 1
+    phi = [i for i in xrange(size)]
+
+    for i in xrange(2, size):
+        if phi[i] == i:
+            phi[i] = i - 1
+            for j in xrange(2*i, size, i):
+                phi[j] = (phi[j]//i)*(i-1) 
+
+    return phi
+
+phi = totient
+
 def lprime_div(n):
     size = n + 1
     lpd = [i for i in xrange(size)]
